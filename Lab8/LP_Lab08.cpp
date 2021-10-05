fd68214587c51420331a84e35ec31d1b2cda4dc0
Kate <82730879+keumul@users.noreply.github.com>
2021-10-05 18:41:21 +0300

    Add files via upload

---
+++ Lab8/LP_Lab08.cpp
@@ -0,0 +1,95 @@
+﻿
+#include"../../../7lab/Lab7/Lab7/Dictionary.h"
+#include "../../../7lab/Lab7/Lab7/stdafx.h"
+
+void main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	try
+	{
+#if (defined (TEST1)+defined (TEST2)+defined (TEST3)+defined (TEST4)+defined (TEST5)+defined (TEST6)+defined (TEST7)+defined (TEST8)+defined (TEST9 ) + defined(TEST_DICTIONARY) > 1)
+#error USE_ONLY_ONE_TEST
+#endif // only one test
+
+#ifdef TEST1 //THROW1 "Create: превышен размер имени словаря"
+		Instance test = Create((char*)"ABBASABSBAASADSSASDSDSDDSDBSB", 5);
+#endif // TEST_CREATE_1
+
+#ifdef TEST2 //THROW2 "Create: превышен размер максимальной ёмкости словаря"
+		Instance test = Create((char*)"TEST", 200);
+#endif // TEST_CREATE_2
+
+#ifdef TEST3 //THROW3 "AddEntry: переполнение словаря"
+		Instance test = Create((char*)"TEST", 0);
+		Entry test0 = { 1,"проверка" };
+		AddEntry(test, test0);
+#endif // TEST_ADDENTRY_1
+
+#ifdef TEST4 //THROW4 "AddEntry: дублирование идентификатора"
+		Instance test = Create((char*)"TEST", 5);
+		Entry test0 = { 1,"проверка" };
+		AddEntry(test, test0);
+		AddEntry(test, test0);
+#endif // TEST_ADDENTRY_2
+
+#ifdef TEST5 //THROW5 "GetEntry: не найден элемент"
+		Instance test = Create((char*)"TEST", 5);
+		Entry test0 = { 1,"проверка" };
+		AddEntry(test, test0);
+		GetEntry(test, 2);
+#endif // TEST_GETENTRY_1
+
+#ifdef TEST6 // THROW6 "DelEntry: не найден элемент"
+		Instance test = Create((char*)"TEST", 5);
+		Entry test0 = { 1,"проверка" };
+		AddEntry(test, test0);
+		DelEntry(test, 2);
+#endif // TEST_DELENTRY_1
+
+#ifdef TEST7 // THROW7 "UpdEntry: не найден элемент"
+		Instance test7 = Create((char*)"TEST", 5);
+		Entry test07 = { 1,"проверка" },
+			test007 = { 2,"проверочка" };
+		AddEntry(test7, test07);
+		UpdEntry(test7, 3, test007);
+#endif // TEST_UPDENTRY_1
+
+#ifdef TEST8 // THROW8 "UpdEntry: дублирование идентификатора"
+		Instance test = Create((char*)"TEST", 5);
+		Entry test0 = { 1,"проверка" },
+			test00 = { 1,"проверочка" };
+		AddEntry(test, test0);
+		UpdEntry(test, 1, test00);
+#endif // TEST_UPDENTRY_1
+
+#ifdef TEST_DICTIONARY
+		Instance d1 = Create((char*)"Преподаватели", 5);
+		Entry e1 = { 1,"Плиско" }, e2 = { 2,"Архипенко" }, e3 = { 3,"Колентионок" }, e4 = { 4, "Бабицкий" }, e5 = { 5, "Хадатович" };
+		AddEntry(d1, e1);
+		AddEntry(d1, e2);
+		AddEntry(d1, e3);
+		AddEntry(d1, e4);
+		Entry getE = GetEntry(d1, 4);
+		DelEntry(d1, 2);
+		Entry newentry1 = { 6, "Иванов" };
+		UpdEntry(d1, 3, newentry1);
+		Print(d1);
+
+		Instance d2 = Create((char*)"Студенты", 5);
+		Entry s1 = { 1,"Мамаева" }, s2 = { 2,"Белашков" }, s3 = { 4,"Хлыстов" };
+		AddEntry(d2, s1);
+		AddEntry(d2, s2);
+		AddEntry(d2, s3);
+		Entry newentry3 = { 3, "Булавский" };
+		UpdEntry(d2, 4, newentry3);
+		Print(d2);
+		Delete(d1);
+		Delete(d2);
+#endif // TEST_DICTIONARY
+	}
+	catch (char* e)
+	{
+		std::cout << e << std::endl;
+	}
+}
---
+++ Lab8/LP_Lab08L.cpp
@@ -0,0 +1,93 @@
+﻿
+#include"../../../7lab/Lab7/Lab7/Dictionary.h"
+#include "../../../7lab/Lab7/Lab7/stdafx.h"
+
+namespace Dictionary
+{
+	Instance Create(char name[DICTNAMEMAXSIZE], int size)
+	{
+		if (strlen(name) > DICTNAMEMAXSIZE) throw (char*)THROW1;
+		if (size > DICTMAXSIZE)				throw (char*)THROW2;
+		
+		Instance object;
+
+		object.name[strlen(name)] = '\0';
+		for (int i = 0; object.name[i] != '\0'; i++)
+		{ object.name[i] = name[i]; }
+
+		object.maxsize = size;
+		object.dictionary = new Entry[size];
+		object.size = 0;
+
+		return object;
+	}
+
+	void AddEntry(Instance& inst, Entry ed)
+	{
+		if (inst.size == inst.maxsize) { throw (char*)THROW3; }
+
+		for (int i = 0; i < inst.size; i++)
+		{
+			if (ed.id == inst.dictionary[i].id) { throw (char*)THROW4; }
+		}
+		inst.dictionary[inst.size] = ed;
+		inst.size++;
+	}
+	void DelEntry(Instance& inst, int id)
+	{
+		for (int i = 0; i < inst.size; i++)
+		{
+			if (inst.dictionary[i].id == id)
+			{
+				for (int i = 0; i < inst.size - 1; i++)
+				{
+					inst.dictionary[i] = inst.dictionary[i + 1];
+				}break;
+			}
+			else if (i == inst.size - 1) throw (char*)THROW6;
+		}
+		inst.size--;
+	}
+	/*Entry GetEntry(Instance inst, int id)
+	{
+		Entry findElement;
+		for (int i = 0; i < inst.size; i++)
+		{
+			if (inst.dictionary[i].id == id)
+			{
+				findElement = inst.dictionary[i];
+				return findElement;
+			}
+		}
+		throw(char*)THROW5;
+	}
+
+	void UpdEntry(Instance& inst, int id, Entry new_ed)
+	{
+		int index = -5;
+		for (int i = 0; i < inst.size; i++)
+		{
+			if (inst.dictionary[i].id == id) index = i;
+			if (inst.dictionary[i].id == new_ed.id) throw (char*)THROW8;
+		}
+		if (index == -5) throw (char*)THROW7;
+		else inst.dictionary[index] = new_ed;
+	}
+
+	void Print(Instance d)
+	{
+		std::cout << "-------" << d.name << "-------" << std::endl;
+		for (int i = 0; i < d.size; i++)
+		{
+			std::cout << d.dictionary[i].id << " ";
+			std::cout << d.dictionary[i].name;
+			std::cout << std::endl;
+		}
+	}
+	void Delete(Instance& d)
+	{
+		delete[] d.dictionary;
+		d.maxsize = NULL;
+		d.size = NULL;
+	}*/
+}
